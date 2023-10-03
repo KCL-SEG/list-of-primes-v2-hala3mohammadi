dd5134a7113cc51437eb6bd47f50e2fdc4014b4e
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-10-03 04:38:46 +0100

    Raise a ValueError for a 0 arguement or a negative number

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    p = 2
+    
+    if number_of_primes <= 0: 
+        raise ValueError(f"0 or negative numbers are not valid")
+    
+    while len(list) < number_of_primes:
+        flag = True
+        for i in range(2, int(p**0.5) + 1):
+            if (p%i==0):
+                flag = False
+                break    
+        if flag:
+            list.append(p)
+        p += 1
+    
     return list
